830519aafc84ba881fbbaf2aacdfe4c2a6d2ce92
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2024-05-16 18:50:31 +0400

    'third'

--- assets/js/cleveland.js
+++ assets/js/cleveland.js
@@ -1,3 +1,30 @@
-function redirectToPage(url) {
+var button = document.querySelector(".ourservicebutton");
+
+
+button.onclick = function() {
+   
+    var url = "https://www.youtube.com/";
+    
+   
     window.location.href = url;
-}
+};
+
+
+
+
+
+
+
+
+
+
+var button = document.querySelector(".contactusbutton");
+
+
+button.onclick = function() {
+   
+    var url = "https://www.youtube.com/";
+    
+   
+    window.location.href = url;
+};
